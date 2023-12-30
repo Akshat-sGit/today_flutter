5d686612e61209b17949e41e9599c3557f08aeb6
Akshat Agarwal <e21cseu0806@bennett.edu.in>
2023-12-30 14:31:19 +0530

    commit

--- lib/screens/task_screen.dart
+++ lib/screens/task_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:todoey_flutter/screens/add_task_screen.dart'; 
+import 'package:todoey_flutter/widgets/task_tile.dart';
 
 class TaskScreen extends StatelessWidget {
   static const String id = 'task_screen';
@@ -90,48 +91,3 @@ class TaskScreen extends StatelessWidget {
   }
 }
 
-class TaskTile extends StatefulWidget {
-  const TaskTile({Key? key, required this.isChecked}) : super(key: key);
-
-  final bool isChecked;
-  
-
-  @override
-  // ignore: library_private_types_in_public_api
-  _TaskTileState createState() => _TaskTileState();
-}
-
-class _TaskTileState extends State<TaskTile> {
-  late bool isChecked = false;
-
-  @override
-  void initState() {
-    super.initState();
-    isChecked = widget.isChecked;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      textColor: Colors.black,
-      title: Text(
-        "This is a task.",
-        style: TextStyle(
-          color: Colors.white, 
-          decoration: isChecked ?  TextDecoration.lineThrough : null, 
-        ),
-      ),
-      trailing: Checkbox(
-        fillColor: MaterialStateProperty.all(Colors.black),
-        checkColor: Colors.white,
-        activeColor: Colors.black,
-        value: isChecked,
-        onChanged: (value) {
-          setState(() {
-            isChecked = value!;
-          });
-        },
-      ),
-    );
-  }
-} 
--- lib/widgets/task_tile.dart
+++ lib/widgets/task_tile.dart
@@ -1,3 +1,47 @@
-class Tile{ 
+import 'package:flutter/material.dart';
+
+class TaskTile extends StatefulWidget {
+  const TaskTile({Key? key, required this.isChecked}) : super(key: key);
+
+  final bool isChecked;
   
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  late bool isChecked = false;
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.isChecked;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      textColor: Colors.black,
+      title: Text(
+        "This is a task.",
+        style: TextStyle(
+          color: Colors.white, 
+          decoration: isChecked ?  TextDecoration.lineThrough : null, 
+        ),
+      ),
+      trailing: Checkbox(
+        fillColor: MaterialStateProperty.all(Colors.black),
+        checkColor: Colors.white,
+        activeColor: Colors.black,
+        value: isChecked,
+        onChanged: (value) {
+          setState(() {
+            isChecked = value!;
+          });
+        },
+      ),
+    );
+  }
 }
